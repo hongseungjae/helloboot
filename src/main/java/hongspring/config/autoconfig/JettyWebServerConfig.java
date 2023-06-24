bdeada2971e5ed70e78a4418673fcb6dcd956273
hongseungjae <ghdtmdwo123@naver.com>
2023-06-24 18:49:00 +0900

    feat: @Conditional을 이용하여 조건에 따라 구성하기

--- src/main/java/hongspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/hongspring/config/autoconfig/JettyWebServerConfig.java
@@ -3,18 +3,21 @@ package hongspring.config.autoconfig;
 import hongspring.config.ConditionalMyOnClass;
 import hongspring.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory serverFactory(){
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
-
 }
